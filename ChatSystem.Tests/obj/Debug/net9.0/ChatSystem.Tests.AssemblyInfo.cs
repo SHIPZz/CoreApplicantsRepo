a70522edd11cf8acd4496a85833d67ae34eb73c7
Kenan Ibragimov <102488775+SHIPZz@users.noreply.github.com>
2025-07-26 18:08:18 +0300

    fix project assembly

--- ChatSystem.Tests/obj/Debug/net9.0/ChatSystem.Tests.AssemblyInfo.cs
+++ ChatSystem.Tests/obj/Debug/net9.0/ChatSystem.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatSystem.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+802cc48838f76269f853994bf7c1dcf05b07a605")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d40ac5f55583b7a1fcd3c3e01b8edf393b6fbf72")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatSystem.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatSystem.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
